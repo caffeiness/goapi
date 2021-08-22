fa7d365859b2cf410aa6780a85473cae03aa4d22
caffeiness <caffeine.konchan@gmail.com>
2021-08-22 22:55:24 +0900

    add show

--- internal/http/api.go
+++ internal/http/api.go
@@ -1,14 +1,9 @@
 package http
 
 import (
-	"bytes"
-	"encoding/base64"
 	"fmt"
-	"html/template"
-	"image"
-	"image/jpeg"
 	"io"
-	"log"
+	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -55,40 +50,10 @@ func upload(c echo.Context) error {
 	return c.HTML(http.StatusOK, fmt.Sprintf("<p>Uploaded successfully %d files with fields name=%s and discription=%s.</p>", len(files), name, discription))
 }
 
-var templates = template.Must(template.ParseFiles("templates/index.html"))
-
 func show(c echo.Context) error {
+	files, _ := ioutil.ReadDir("img")
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 	return c.HTML(http.StatusOK, "OK")
-}
-func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-	if err := templates.ExecuteTemplate(w, tmpl+".html", data); err != nil {
-		log.Fatalln("Unable to execute template.")
-	}
-}
-
-func ShowHandler(w http.ResponseWriter, r *http.Request) {
-	file, err := os.Open("/tmp/test.jpg")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer file.Close()
-
-	img, _, err := image.Decode(file)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	writeImageWithTemplate(w, "show", &img)
-}
-
-func writeImageWithTemplate(w http.ResponseWriter, tmpl string, img *image.Image) {
-	buffer := new(bytes.Buffer)
-	if err := jpeg.Encode(buffer, *img, nil); err != nil {
-		log.Fatalln("Unable to encode image.")
-	}
-	str := base64.StdEncoding.EncodeToString(buffer.Bytes())
-	data := map[string]interface{}{"Title": tmpl, "Image": str}
-	renderTemplate(w, tmpl, data)
 }
